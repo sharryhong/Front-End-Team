7b8fda02e98ff822698053b7c16d4f18029ca2a7
sharry-hong <sharry.hong@kakaoCorp.com>
2019-04-08 10:12:57 +0900

    변수명 좀 더 명확하게 변경

--- study-sharry/01-inputNumber/counter-sharry.js
+++ study-sharry/01-inputNumber/counter-sharry.js
@@ -2,28 +2,28 @@
 "use strict";
 
 (function () {
-    var count = document.getElementById("count");
+    var inputCount = document.getElementById("inputCount");
     var num = 0;
-    count.value = num; // input 값에 set
+    inputCount.value = num; // input 값에 set
 
-    count.addEventListener("keyup", function(event) {
+    inputCount.addEventListener("keyup", function(event) {
       inputOnlyNumber(event); // 우선 숫자만 입력하는 기능을 가진 함수로 체크합니다.
       // input 값을 get하여 num 변수에 set
       // input value는 string이므로 number로 형변환 // 01 -> 1 대응
-      num = Number(count.value);
-      if (num !== 0) count.value = num;
+      num = Number(inputCount.value);
+      if (num !== 0) inputCount.value = num;
     });
 
     // +버튼 클릭 시 1씩 증가
     document.getElementById("btnPlus").addEventListener("click", function () {
       num++;
-      count.value = num;
+      inputCount.value = num;
     });
 
     // - 버튼 클릭 시 1씩 감소
     document.getElementById("btnMinus").addEventListener("click", function () {
       num--;
-      count.value = num;
+      inputCount.value = num;
     });
 
 })();
@@ -31,7 +31,7 @@
 // 숫자만 입력되게 하는 기능 함수
 // if (code > 47 && code < 58) 방법과 차이점
 // regex(정규표현식)에 대해 알아봅시다.
-// 현재 이슈 : 마이너스도 삭제됩니다. 키키 
+// 현재 이슈 : 마이너스도 삭제됩니다. 키키
 function inputOnlyNumber(event) {
    var code = event.keyCode || event.which;
    if (code == 8 || code == 46 || code == 37 || code == 39) return;
