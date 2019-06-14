7e06810117e41e2784f766f889b758cb3f83bdf1
sonia.kim <sonia.kim@kakaocorp.com>
2019-06-14 15:53:03 +0900

    [1차 과제] 글자수 체크 코드 리팩토링

--- studyCoP01/ex01-base/sonia-ex01.js
+++ studyCoP01/ex01-base/sonia-ex01.js
@@ -1,12 +1,11 @@
 // 텍스트 입력 수 구하기
- var text = document.querySelector("#tf");
- var textValue = text.value;
- var textLength = textValue.length;
+ var textArea = document.querySelector("#textArea");
 
- // 구한 갯수 카운터에 넣기
- var count = document.querySelector('#countText');
- var countVal = textLength;
- count.value = countVal;
- count.innerHTML = textLength;
-
- // 해결사항 : html에 입력했을 때만 카운트가 됨. 입력창에 입력한 밸류값 받아와야 하는데..
+ textArea.addEventListener('keydown', function() {
+    var textValue = textArea.value;
+    var textLength = textValue.length;
+    
+    // 구한 갯수 카운터에 넣기
+    var countText = document.querySelector('#countText');
+    countText.textContent = textLength;
+});
