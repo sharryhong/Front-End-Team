b3bac32a43102e6267266c53afef5e311d5be9f7
soo(배수현) <soo.bae@dktechin.com>
2019-09-30 11:56:43 +0900

    객체 다시 적용

--- studyCoP01/ex08_object/ex08_object_soo.js
+++ studyCoP01/ex08_object/ex08_object_soo.js
@@ -1,22 +1,22 @@
 'use strict';
 (function(){
-    let gugudan = {};
     let results = [];
     let length = 9;
 
-   gugudan.calculate = function(i){
-       for(let j=1; j<=length; j++){
-           results[j-1] = i *j;
-       }
-   }
-
-   gugudan.print = function(i,results){
-       document.write(i+ "단<br>")
-        for(var j=1; j<=length; j++){
-            document.write(i+ " * " +j+ " = " + results[j-1]+"<br>");
+    let gugudan = {
+        calculate: function(i){
+         for(let j=1; j<=length; j++){
+             results[j-1] = i *j;
+             }
+        },
+        print: function(i,results){
+         document.write(i+ "단<br>")
+             for(var j=1; j<=length; j++){
+                 document.write(i+ " * " +j+ " = " + results[j-1]+"<br>");
+             }
         }
-   }
-
+    }
+    
    for(let i=2; i<=length; i++){
         gugudan.calculate(i);
         gugudan.print(i,results);
