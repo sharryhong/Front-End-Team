a40363ce0b12a06a39a7f23876af4137d0db0a94
soo(배수현) <soo.bae@dktechin.com>
2019-11-26 17:50:12 +0900

    for문 띄어쓰기 리팩토링

--- studyCoP01/ex08_object/ex08_object_soo.js
+++ studyCoP01/ex08_object/ex08_object_soo.js
@@ -5,19 +5,19 @@
 
     let gugudan = {
         calculate: function(i){
-         for(let j=1; j<=length; j++){
+         for(let j = 1; j <= length; j++){
              results[j-1] = i *j;
              }
         },
         print: function(i,results){
          document.write(i+ "단<br>")
-             for(var j=1; j<=length; j++){
+             for(var j = 1; j <= length; j++){
                  document.write(i+ " * " +j+ " = " + results[j-1]+"<br>");
              }
         }
     }
     
-   for(let i=2; i<=length; i++){
+   for(let i = 2; i <= length; i++){
         gugudan.calculate(i);
         gugudan.print(i,results);
     }
